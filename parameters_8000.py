2b619cc49cb951c31ca025fb7672b2ea95222e14
Amauri Rossetti Giovani <amaurirg@terra.com.br>
2017-02-24 08:13:44 -0300

    httpserver.log

--- parameters_8000.py
+++ parameters_8000.py
@@ -1,1 +1,1 @@
-password="pbkdf2(1000,20,sha512)$ba8ebd739be86f03$61a5de06a549401c0aada1fb0d071f793dce0f37"
+password="pbkdf2(1000,20,sha512)$ac2da141e64b740d$b6736be6b23456d581b748c9a51f60b55fea6cd7"
