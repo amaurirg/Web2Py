235571cd2273a858cbc8f291731672eadf6b8206
Amauri Rossetti Giovani <amaurirg@terra.com.br>
2017-06-20 23:28:21 -0300

    sessions

--- parameters_8000.py
+++ parameters_8000.py
@@ -1,1 +1,1 @@
-password="pbkdf2(1000,20,sha512)$bd58e6cbbf29c0be$35144fb0d4cb862ce16ac6cd71dab74dd9832eed"
+password="pbkdf2(1000,20,sha512)$91182bef60d278cf$f192ba63e308e466f8b0d544232adfe0667c6274"
